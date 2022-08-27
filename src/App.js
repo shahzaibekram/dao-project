186f76c67c76d22e6c4e27242e3ea63ff2419c3f
Shahzaib Ekram <shahzebekram2468@gmail.com>
2022-08-28 04:16:03 +0500

    metamask

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import './App.css';
 import Header from './Components/Header';
-// import Demo from './Pages/Demo';
 import Demo from "./Pages/Demo";
 import Proposal from "./Pages/Proposal";
 import NewProposal from "./Pages/NewProposal";
-
 
 const style = {
   wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2D242F]
@@ -21,7 +19,7 @@ function App() {
         <Route path="/" element={<Demo />} />
         <Route path="/proposal" element={<Proposal />} />
         <Route path="/new-proposal" element={<NewProposal />} />
-        {/* <Route path="/proposal" element={<Proposal />} /> */}
+        {/* <Route path="/data" element={<Data />} /> */}
       </Routes>
     </div>
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,73 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React,{useState} from 'react'
+import { Link } from 'react-router-dom';    
+import { ethers } from 'ethers';
 
 const style = {
     wrapper: `p-4 w-screen flex justify-between items-center `,
-    nav: `flex-1 flex justify-center items-center ml-12`,
+    nav: `flex-1 flex justify-center items-center`,
     navItemsContainer: `flex bg-[#191B1F] rounded-3xl `,
     navItem: `px-3 py-1 m-1 flex items-center text-lg hover:bg-sky-700`,
     buttonsContainer: `bg-[#191B1F] rounded-3xl hover:bg-sky-700`,
     btn: `px-3 py-2`,
+    btnTextContainer: `h-8 flex item-center`
 }
 
 const Header = () => {
+
+     // usetstate for storing and retrieving wallet details
+  const [data, setdata] = useState({
+    address: "",
+    Balance: null,
+  });
+  
+  // Button handler button for handling a
+  // request event for metamask
+  const btnhandler = () => {
+  
+    // Asking if metamask is already present or not
+    if (window.ethereum) {
+  
+      // res[0] for fetching a first wallet
+      window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((res) => accountChangeHandler(res[0]));
+    } else {
+      alert("install metamask extension!!");
+    }
+  };
+  
+  // getbalance function for getting a balance in
+  // a right format with help of ethers
+  const getbalance = (address) => {
+  
+    // Requesting balance method
+    window.ethereum
+      .request({ 
+        method: "eth_getBalance", 
+        params: [address, "latest"] 
+      })
+      .then((balance) => {
+        // Setting balance
+        setdata({
+          Balance: ethers.utils.formatEther(balance),
+        });
+      });
+  };
+  
+  // Function for getting handling all events
+  const accountChangeHandler = (account) => {
+    // Setting an address data
+    setdata({
+      address: account,
+    });
+  
+    // Setting a balance
+    getbalance(account);
+  };
+
     return (
         <div className={style.wrapper}>
-            <div className={style.nav}>
+            <div className={style.nav} style={{marginLeft: '14%'}}>
                 <div className={style.navItemsContainer}>
                     <div className= {style.navItem} >
                         <Link  to='/'>DAO</Link>
@@ -24,7 +78,7 @@ const Header = () => {
                 </div>
             </div>
             <div className={style.buttonsContainer}>
-                <button className={style.btn}>Connected Wallet</button>
+                <button className={`${style.btn}`} onClick={btnhandler}>Wallet{`= ${data.Balance}`}</button>
             </div>
         </div>
     )
